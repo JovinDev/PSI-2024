89a165eb0488906f27348cb0d87a74a33a142827
JOÃO VINICIUS SILVA DOS SANTOS <20211011110021@ifrn.local>
2024-08-02 10:13:00 -0300

    slide 3 finished

--- loja/admin.py
+++ loja/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
-
+from django.contrib import admin #isso já vai estar existindo no arquivo
 # Register your models here.
+from .models import *
+admin.site.register(Fabricante)
+admin.site.register(Categoria)
+admin.site.register(Produto)
