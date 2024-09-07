f5d8fa73ac9aa7570f13bb4c3cc3994aca5608c6
JovinDev <joo.vinicius2005@gmail.com>
2024-09-07 14:33:29 -0300

    IDK

--- loja/admin.py
+++ loja/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin #isso já vai estar existindo no arquivo
 # Register your models here.
 from .models import *
+class FabricanteAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+
+admin.site.register(Fabricante, FabricanteAdmin)
 admin.site.register(Fabricante)
 admin.site.register(Categoria)
 admin.site.register(Produto)
+
