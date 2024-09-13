5f85a2c7d6037288ee101b3aa2fde10d66042cbb
JovinDev <joo.vinicius2005@gmail.com>
2024-09-13 11:46:21 -0300

    Pag 55

--- loja/views/ProdutoView.py
+++ loja/views/ProdutoView.py
@@ -1,20 +1,23 @@
-from django.http import HttpResponse
+from django.shortcuts import render # Retire from django.http import HttpResponse
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
+def edit_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = { 'produto': produto }
+    return render(request, template_name='produto/produto-edit.html', context=context, status=200)
 def list_produto_view(request, id=None):
-    dias = request.GET.get("dias")
     produto = request.GET.get("produto")
     destaque = request.GET.get("destaque")
     promocao = request.GET.get("promocao")
     categoria = request.GET.get("categoria")
     fabricante = request.GET.get("fabricante")
-
+    dias = request.GET.get("dias")
     produtos = Produto.objects.all()
-    if dias is not None:
-        now = timezone.now()
-        now = now - timedelta(days = int(dias))
-        produtos = produtos.filter(criado_em__gte=now)
     if produto is not None:
         produtos = produtos.filter(Produto__contains=produto)
     if promocao is not None:
@@ -25,9 +28,16 @@ def list_produto_view(request, id=None):
         produtos = produtos.filter(categoria__Categoria=categoria)
     if fabricante is not None:
         produtos = produtos.filter(fabricante__Fabricante=fabricante)
+    if dias is not None:
+        now = timezone.now()
+        now = now - timedelta(days = int(dias))
+        produtos = produtos.filter(criado_em__gte=now)
     if id is not None:
         produtos = produtos.filter(id=id)
     print(produtos)
+    # Adicione para definir o contexto e carregar o template
+    context = { 'produtos': produtos }
+    return render(request, template_name='produto/produto.html',context=context, status=200)
 
     if id is None:
         return HttpResponse('<h1>Nenhum id foi informado</h1>')
