c0eb344d256b55dbfd998cac93f2c02a3217e6a8
JovinDev <joo.vinicius2005@gmail.com>
2024-11-10 19:55:58 -0300

    domingo a noite...

--- loja/admin.py
+++ loja/admin.py
@@ -16,5 +16,5 @@ class ProdutoAdmin(admin.ModelAdmin):
 admin.site.register(Fabricante,FabricanteAdmin)
 admin.site.register(Categoria)
 admin.site.register(Produto, ProdutoAdmin)
-
+admin.site.register(Usuario)
 
