7bafaace1f1d43d3599c4b0dd778db18dcdc9e21
JOÃO VINICIUS SILVA DOS SANTOS <20211011110021@ifrn.local>
2025-01-31 10:15:13 -0300

    Capitulo 20 finalizado

--- loja/models/__init__.py
+++ loja/models/__init__.py
@@ -10,3 +10,7 @@ PERFIL=(
     (2, 'Usuario')
 )
 from .Usuario import Usuario
+from .Usuario import Usuario
+from .Usuario import Usuario
+from .Carrinho import Carrinho
+from .Carrinho import CarrinhoItem
